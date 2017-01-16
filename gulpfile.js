95c87c4b42fbc7551343ebf3961bfcda15b9dee3
coomysky <coomysky@gmail.com>
2017-01-16 18:54:24 +0800

    add clean dest into watch task

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ const gulpAmpValidator = require('gulp-amphtml-validator');
 const replace = require('gulp-replace');
 const fs = require('fs');
 const gulpSass = require('gulp-sass');
+const cleanDest = require('gulp-clean-dest');
 
 gulp.task('styles', function () {
     gulp.src('./scss/*.scss')    // 指定要處理的 Scss 檔案目錄
+        .pipe(cleanDest('./css'))
         .pipe(gulpSass())         // 編譯 Scss
         .pipe(gulp.dest('./css'));  // 指定編譯後的 css 檔案目錄
 });
@@ -21,7 +23,7 @@ gulp.task('amp:test', () => {
   return gulp.src('*.html')
     // Valide the input and attach the validation result to the "amp" property
     // of the file object.
-
+    .pipe(cleanDest('./build'))
     .pipe(replace(/<link href="([^\.]+\.css)"[^>]*>/g, function(s, filename) {
       var style = fs.readFileSync('./css/'+filename, 'utf8');
       return '<style amp-custom>\n' + style + '\n</style>';
